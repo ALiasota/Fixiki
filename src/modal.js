0bfbea8a4fe005e0e17fd3c6002d55618a40246c
Andrii Liasota <lyasota.andriy@gmail.com>
2021-12-06 18:22:19 +0200

    modal-fix

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn2: document.querySelector('[data-modal-open2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal2);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleModal2() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
