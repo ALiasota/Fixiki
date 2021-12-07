26e9a6a8c48b01e152758b54ee6a783fbb85c3eb
zh.vasylevska <zhanna.vasilevskaya@gmail.com>
2021-12-08 00:08:53 +0200

    fix styles for author name and add grey dots

--- src/slider.js
+++ src/slider.js
@@ -3,7 +3,7 @@ $('.slider-list').slick({
   slidesToScroll: 1,
   autoplay: true,
   arrows:true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 20000,
   dots:true,
   dotsClass:'slider__dots',
   appendDots:$('.slider-dots__wrap'),
