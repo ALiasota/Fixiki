97acf2839c3c7920f8a434999299649448cdf196
Sasha Hulko <hpalex555@gmail.com>
2021-12-07 22:56:24 +0300

    do-did-done

--- src/location.js
+++ src/location.js
@@ -1,23 +1,15 @@
 (() => {
   const refs = {
-     locationBtnRef: document.querySelector("[data-open-location]"),
-     locationCloseRef: document.querySelector("[location-close-button]"),
-     locationWind: document.querySelector("[data-location]")
+    locationBtnRef: document.querySelector("[data-open-location]"),
+    locationCloseRef: document.querySelector("[location-close-button]"),
+    locationWind: document.querySelector("[data-location]")
   }
 
   refs.locationBtnRef.addEventListener('click', toggleLocation); 
-  // refs.locationCloseRef.addEventListener('click', toggleLocation);
+  refs.locationCloseRef.addEventListener('click', toggleLocation);
 
   function toggleLocation() {
-    refs.locationWind.classList.toggle('is-open1');
- }
+    refs.locationWind.classList.toggle('is-opened');
+}
 })();
-  // menuBtnRef.addEventListener("click", () => {
-  //   const expanded =
-  //     locationBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  //   locationBtnRef.classList.toggle("is-open");
-  //   locationBtnRef.setAttribute("aria-expanded", !expanded);
-
-  //   locationCloseRef.classList.toggle("is-open");
-  //   locationWind.classList.toggle("location-open");
